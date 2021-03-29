56cb7143636ad878f3229c254cef2f692b2dea4b
Alberto Chávez Pérez <alberto@francisco.nu>
2021-03-30 00:05:23 +0200

    Removal of .d.ts files

--- TS-library/src/Button.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="react" />
-interface IButton {
-    text: string;
-}
-export declare function Button(props: IButton): JSX.Element;
-export {};
--- TS-library/src/Hello.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-interface IProps {
-    name: string;
-}
-export declare class Hello extends React.Component<IProps, null> {
-    render(): JSX.Element;
-}
-export {};
